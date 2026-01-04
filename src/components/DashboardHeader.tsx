546adbdb3fe02cecaa64865771c2f7d00bbbb3c9
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2026-01-04 11:11:23 +0600

    feat(dashboard): integrate user information in DashboardHeader and enhance avatar display

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,18 +1,27 @@
+"use client";
+
 import { Bell } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useAppSelector } from "@/redux/hooks";
 
 interface DashboardHeaderProps {
   title: string;
 }
 
 export default function DashboardHeader({ title }: DashboardHeaderProps) {
+  const { user } = useAppSelector((state) => state.auth);
+
   return (
     <div className="bg-card px-4 py-4 rounded-2xl mb-4">
       <div className="flex items-center justify-between">
         <h2 className="text-3xl font-medium text-[#3B3B3B] mb-4">{title}</h2>
         <div className="flex items-center gap-4">
-          <Button variant="ghost" size="icon" className="relative hover:text-blue-600 hover:bg-[#F2F7FF]">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="relative hover:text-blue-600 hover:bg-[#F2F7FF]"
+          >
             <Bell className="w-5 h-5" />
           </Button>
 
@@ -20,15 +29,19 @@ export default function DashboardHeader({ title }: DashboardHeaderProps) {
             <Avatar className="w-10 h-10">
               <AvatarImage
                 src="/placeholder.svg?height=40&width=40"
-                alt="Jhon Marcel"
+                alt={user?.full_name}
               />
-              <AvatarFallback>JM</AvatarFallback>
+              <AvatarFallback className="font-bold uppercase">{user?.full_name?.slice(0, 1)}</AvatarFallback>
             </Avatar>
             <div className="flex flex-col tex-[#3B3B3B]">
-              <span className="font-medium text-xl">
-                Jhon Marcel
+              <span className="font-medium text-xl">{user?.full_name}</span>
+              <span className="text-xs text-muted-foreground">
+                {user?.role === "operations"
+                  ? "Operator"
+                  : user?.role === "executive"
+                  ? "Executive"
+                  : "Marketing Manager"}
               </span>
-              <span className="text-xs text-muted-foreground">Operator</span>
             </div>
           </div>
         </div>
